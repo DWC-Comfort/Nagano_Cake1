e355bf95c0416eda4e8af90eb2158770dadf19a6
kozu11 <nk11.kzdl20@gmail.com>
2021-12-18 13:35:38 +0000

    [fix]header

---
+++ db/migrate/20211218123418_add_columns_to_receivers.rb
@@ -0,0 +1,8 @@
+class AddColumnsToReceivers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :receivers, :customer_id, :integer
+    add_column :receivers, :name, :string
+    add_column :receivers, :postal_code, :string
+    add_column :receivers, :
+  end
+end
