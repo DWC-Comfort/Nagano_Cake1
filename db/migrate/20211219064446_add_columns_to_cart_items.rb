5c60793b44d6bc907bfd04d141c5d6fc4c8c1b4e
KatTakahashi <t.ka.ka.t.11@gmail.com>
2021-12-19 12:52:24 +0000

    [Update]cartitems

--- db/migrate/20211219064446_add_columns_to_cart_items.rb
+++ db/migrate/20211219064446_add_columns_to_cart_items.rb
@@ -2,6 +2,6 @@ class AddColumnsToCartItems < ActiveRecord::Migration[5.2]
   def change
     add_column :cart_items, :customer_id, :integer
     add_column :cart_items, :item_id, :integer
-    add_column :carttems, :quantity, :integer
+    add_column :cart_items, :quantity, :integer
   end
 end
