71eb298dda127390d58be906e4fe8b74f9464272
hkrhrsk <hkrhrsk@gmail.com>
2021-12-20 03:12:21 +0000

    createitem

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,15 +10,29 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+      flash[:notice] = "商品を追加しました"
+    else
+      render "new"
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :image_id, :name, :introduction, :price, :is_active)
   end
 
 end
