bf0713e7621cf890f54a9d8eac8ae9781c251898
hkrhrsk <hkrhrsk@gmail.com>
2021-12-21 14:24:36 +0000

    kaminari

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,9 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.all.page(params[:page]).per()
+    @items = Item.all.page(params[:page]).per(8)
   end
-  
+
   def show
      @items = Item.all
      @item = Item.find(params[:id])
