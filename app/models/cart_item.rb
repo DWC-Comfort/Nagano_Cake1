cf40235d9c2795f09d6580b481b18e07aab283fd
KatTakahashi <t.ka.ka.t.11@gmail.com>
2021-12-22 11:30:01 +0000

    [Add]Cart_item

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,16 @@
 class CartItem < ApplicationRecord
+  
+  belongs_to :item
+  belongs_to :customer
+  
+  #税込み価格用
+  def add_tax_price
+   (price * 1.1).floor
+  end
+  
+  #小計算出用
+  def subtotal
+    item.add_tax_price * quantity
+  end
+  
 end
