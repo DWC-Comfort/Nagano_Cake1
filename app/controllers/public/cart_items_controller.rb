df83fd934d8ca3e72bb1ae6db8ab42e6810adb06
KatTakahashi <t.ka.ka.t.11@gmail.com>
2021-12-22 12:36:01 +0000

    [Update]link_to 注文情報入力画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,35 +4,34 @@ class Public::CartItemsController < ApplicationController
     @cart_items = current_customer.cart_items.all
     @total_price = 0  #合計金額算出用
   end
-  
+
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    #if Item.find_by(name: params[:item][:name])
-      #@cart_item.update(cart_item_params)
-    #else
+    if Item.find_by(item: @cart_item.item)
+      @cart_item.update(cart_item_params)
+    else
       @cart_item.save
-    #end
-    @cart_item.save
-    redirect_to cart_items_path, notice: "You have added cart_item successfully."
+    end
+        redirect_to cart_items_path, notice: "You have added cart_item successfully."
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path, notice: 'You have updated cart_item successfully.'
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path, notice: "You have deleted cart_item successfully."
   end
-  
+
   def destroy_all
     CartItem.destroy_all
     redirect_to cart_items_path, notice: "You have deleted cart_items successfully."
   end
-  
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
