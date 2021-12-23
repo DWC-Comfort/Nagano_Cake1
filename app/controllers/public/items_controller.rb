c7f70bdb41c7aa8a940604fa40ada0ceffd74819
hkrhrsk <hkrhrsk@gmail.com>
2021-12-23 07:10:39 +0000

    viewchange

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,12 +2,13 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.all.page(params[:page]).per(8)
+    @allitems = Item.all.count
   end
 
   def show
-     @items = Item.all
-     @item = Item.find(params[:id])
-     @cart_item = CartItem.new
+    @items = Item.all
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
